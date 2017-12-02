79e37b61b5992f8a7812f7c9c688918b08325c1c
Phillips <mtphill@8c85901720e8.ant.amazon.com>
2017-12-01 18:35:19 -0800

    added basic verification upon login

--- WATrailsDatabase/src/LoginGUI.java
+++ WATrailsDatabase/src/LoginGUI.java
@@ -4,14 +4,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.*;
 
+import static javax.swing.SwingConstants.CENTER;
 
 /**
  * A login frame for choosing user type.
@@ -27,28 +22,57 @@ public class LoginGUI extends JFrame {
 
     private UserType userType;
 
+    private JLabel nameLabel;
+    private JTextField nameEntry;
+
+    private JLabel couldNotFindLabel;
+
     public LoginGUI() {
-        setSize(new Dimension(400, 300));
+        setSize(new Dimension(500, 300));
         userType = UserType.NONE;
-        addButtons();
+
+        addComponents();
+
         setVisible(true);
     }
 
-    public void addButtons() {
+    private void addComponents() {
         JPanel panel = new JPanel();
-        panel.add(new JLabel("Please select a login type"), JLabel.CENTER);
-        panel.setLayout(new GridLayout(3,0));
-        JButton hiker = new JButton("Hiker");
+        panel.setLayout(new GridLayout(7,0));
 
-        panel.add(hiker);
+        JLabel welcome = new JLabel("Welcome to WA State Trails Database!", JLabel.CENTER);
+        welcome.setForeground(Color.BLUE);
+        welcome.setFont(new Font("WelcomeFont", Font.ITALIC, 18));
+        panel.add(welcome);
+
+        nameLabel = new JLabel("Please enter your name.", JLabel.CENTER);
+        nameEntry = new JTextField(50);
+        panel.add(nameLabel);
+        panel.add(nameEntry);
+
+        JLabel select = new JLabel("Are you a Hiker or a PathFinder?", JLabel.CENTER);
+        panel.add(select);
+        JButton hiker = new JButton("Hiker");
         JButton pathfinder = new JButton("Pathfinder");
         panel.add(pathfinder);
+        panel.add(hiker);
+
+        couldNotFindLabel = new JLabel("Could not find user. Please try again.", JLabel.CENTER);
+        couldNotFindLabel.setVisible(false);
+        couldNotFindLabel.setForeground(Color.RED);
+        panel.add(couldNotFindLabel);
 
         hiker.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 userType = UserType.HIKER;
-                firePropertyChange("UserType", 0, userType);
+                if (verifyUser(userType)) {
+                    firePropertyChange("UserType", 0, userType);
+                    couldNotFindLabel.setVisible(false);
+                } else {
+                    // Add login failed message.
+                    couldNotFindLabel.setVisible(true);
+                }
             }
         });
 
@@ -56,7 +80,13 @@ public class LoginGUI extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 userType = UserType.PATHFINDER;
-                firePropertyChange("UserType", 0, userType);
+                if (verifyUser(userType)) {
+                    firePropertyChange("UserType", 0, userType);
+                    couldNotFindLabel.setVisible(false);
+                } else {
+                    // Add login failed message.
+                    couldNotFindLabel.setVisible(true);
+                }
             }
         });
 
@@ -64,13 +94,62 @@ public class LoginGUI extends JFrame {
     }
 
     /**
+     * Should verify a user exists to log them in.
+     */
+    public boolean verifyUser(UserType userType) {
+        Statement stmt = null;
+
+        String query = null;
+        if (userType.equals(UserType.PATHFINDER)) {
+            query = "select p.pathFinderId from trails.PathFinder p"
+                    + " join trails.Hiker h on p.hikerId = h.hikerId"
+                    + " where name = ?";
+        } else {
+            query = "select name from trails.Hiker where name = ?";
+        }
+
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, nameEntry.getText());
+            ResultSet rs = preparedStatement.executeQuery();
+            return rs.next();
+        } catch (SQLException e) {
+            System.out.println("LoginGUI: Caught SQL error.");
+            System.out.println(e.getMessage());
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException s) {
+                    System.out.println("LoginGUI: Closing statement failed.");
+                    System.out.println(s.getMessage());
+                }
+            }
+        }
+
+        return false;
+    }
+    /**
      * Representatives of user types. Decides what view is shown in the main frame.
      */
     public enum UserType {
-        NONE,
+        NONE(""),
 
-        HIKER,
+        HIKER("Hiker"),
 
-        PATHFINDER
+        PATHFINDER("PathFinder");
+
+        private String table;
+
+        /**
+         * @param table name to store entity in.
+         */
+        private UserType(String table) {
+            this.table = table;
+        }
+
+        public String getTable() {
+            return table;
+        }
     }
 }
